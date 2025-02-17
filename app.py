b32b26ffa3674d2b2014202180c2f0d5645f91e9
MALASANI KARTHIK <karthikmalasani21@gmail.com>
2025-02-17 15:20:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,6 +55,7 @@ def main():
 
         # Calculate total marks
         df["Total Marks"] = df[["Test 1", "Test 2", "Test 3", "Test 4"]].sum(axis=1).astype(int)
+        df["Total Marks"] = df["Total Marks"]*(3/4)
 
         # Display the entire data with total marks
         st.write("### Student Data with Total Marks")
@@ -62,7 +63,7 @@ def main():
 
         # Create a report structure
         attendance_ranges = ["≥75%", "≥65% - <75%", "≥50% - <65%", "<50%"]
-        columns = ["<=20", ">20 & <=40", ">40 & <=60", ">60 & <=70", ">70 & <=80", "Total"]
+        columns = ["<=10", ">10 & <=20", ">20 & <=30", ">30 & <=40", ">40 & <=60", "Total"]
 
         # Initialize report data with zeros
         report_data = {col: [0] * len(attendance_ranges) for col in columns}
@@ -72,16 +73,16 @@ def main():
             group = row["Group"]
             total_marks = row["Total Marks"]
 
-            if total_marks <= 20:
-                column = "<=20"
-            elif 20 < total_marks <= 40:
-                column = ">20 & <=40"
+            if total_marks <= 10:
+                column = "<=10"
+            elif 10 < total_marks <= 20:
+                column = ">10 & <=20"
+            elif 20 < total_marks <= 30:
+                column = ">20 & <=30"
+            elif 30 < total_marks <= 40:
+                column = ">30 & <=40"
             elif 40 < total_marks <= 60:
                 column = ">40 & <=60"
-            elif 60 < total_marks <= 70:
-                column = ">60 & <=70"
-            elif 70 < total_marks <= 80:
-                column = ">70 & <=80"
             else:
                 continue
 
